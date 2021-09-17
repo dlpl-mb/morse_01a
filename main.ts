c25aa661aed89bf860cf2949b37b44c51249b9e9
Denken lernen - Probleme lösen <83607557+dlpl-mb@users.noreply.github.com>
2021-09-17 23:00:20 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,33 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("-")
+})
+basic.showLeds(`
+    . . # . .
+    # # # # #
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
